593d3a66685f1fff330c148e9feacc41a73ae970
lucasnagio <lucasangio7@gmail.com>
2021-02-10 12:21:24 -0300

    prueba versionado

--- Main.java
+++ Main.java
@@ -3,8 +3,11 @@ import java.util.Collections;
 
 public class Main {
     public static void main(String[] args)
-    {
 
+    {
+        //prueba versionado
+        //
+        int numero=0;
     }
 
 }
